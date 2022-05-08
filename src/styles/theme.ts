2e2e0cea6219c727b0e8447a50d6d3377bc48d06
wunlei <confusioncosmos@gmail.com>
2022-05-08 21:38:33 +0300

    feat: update theme styles

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -3,4 +3,28 @@ export const theme = createTheme({
   typography: {
     fontFamily: ['Manrope', 'sans-serif'].join(','),
   },
+  palette: {
+    primary: {
+      main: '#212121',
+      light: '#4d4d4d',
+      dark: '#171717',
+    },
+  },
+  components: {
+    MuiFormHelperText: {
+      styleOverrides: {
+        root: {
+          fontSize: '1rem',
+        },
+      },
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: 'none',
+          boxShadow: 'none',
+        },
+      },
+    },
+  },
 });
