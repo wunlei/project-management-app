f4fd46b13183d91a36d33f1340380cd1b2d4fc91
wunlei <confusioncosmos@gmail.com>
2022-05-09 04:24:04 +0300

    feat: add projects page layout

--- src/pages/Projects/ProjectsPage.tsx
+++ src/pages/Projects/ProjectsPage.tsx
@@ -1,5 +1,56 @@
+import {
+  Box,
+  Button,
+  Container,
+  Stack,
+  TextField,
+  Typography,
+} from '@mui/material';
+import { ReactComponent as SearchIcon } from 'assets/icons/search.svg';
+import ProjectCard from 'components/ProjectCard/ProjectCard';
+
 function ProjectsPage() {
-  return <div>Projects Page</div>;
+  return (
+    <Box display="flex" flexDirection="column" minHeight="100vh">
+      <Container component="main" sx={{ flexGrow: 1 }}>
+        <Stack spacing={3}>
+          <Typography variant="h2" fontWeight="bold">
+            Projects
+          </Typography>
+          <Stack direction="row" alignItems="center" spacing={1}>
+            <TextField
+              id="search-input"
+              label="Search"
+              variant="outlined"
+              type="text"
+              sx={{
+                backgroundColor: 'rgba(0, 0, 0, 0.06)',
+                maxWidth: '300px',
+                width: '100%',
+              }}
+            />
+            <Button
+              variant="contained"
+              endIcon={<SearchIcon />}
+              sx={{ lineHeight: '3em' }}
+            >
+              Search
+            </Button>
+          </Stack>
+          <Box
+            sx={{
+              display: 'flex',
+              flexWrap: 'wrap',
+              columnGap: '1rem',
+              rowGap: '1rem',
+            }}
+          >
+            <ProjectCard color={'pink'} title={'My Project #1'}></ProjectCard>
+          </Box>
+        </Stack>
+      </Container>
+    </Box>
+  );
 }
 
 export default ProjectsPage;
