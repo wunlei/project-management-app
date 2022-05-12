44fd0bfbac60225cee867a7a2b73b50b4fef7474
NiNoX14 <hardstuckd5lul@gmail.com>
2022-05-12 22:41:31 +0300

    feat: user page form for edtiting user and reusable PasswordInput component with show/hide  feature

---
+++ src/components/PasswordInput/PasswordInput.tsx
@@ -0,0 +1,98 @@
+import { useState } from 'react';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormHelperText from '@mui/material/FormHelperText';
+import FormControl from '@mui/material/FormControl';
+import IconButton from '@mui/material/IconButton';
+import { ReactComponent as Visibility } from 'assets/svg/eye.svg';
+import { ReactComponent as VisibilityOff } from 'assets/svg/eye-off.svg';
+import { Control, Controller, Path } from 'react-hook-form';
+
+interface Props<T> {
+  control: Control<T>;
+  name: Path<T>;
+}
+
+interface State {
+  password: string;
+  showPassword: boolean;
+}
+/**
+ * @name should correspond to `password` key in your form type
+ */
+function PasswordInput<T /* extends { password: string } */>(props: Props<T>) {
+  const { control, name } = props;
+
+  const [values, setValues] = useState<State>({
+    password: '',
+    showPassword: false,
+  });
+
+  const handleChange =
+    (prop: keyof State) =>
+    (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+      setValues({ ...values, [prop]: event.target.value });
+    };
+
+  const handleClickShowPassword = () => {
+    setValues({
+      ...values,
+      showPassword: !values.showPassword,
+    });
+  };
+
+  const handleMouseDownPassword = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault();
+  };
+
+  return (
+    <Controller
+      control={control}
+      name={name}
+      render={({
+        field: { onChange, onBlur, name, ref },
+        fieldState: { error },
+      }) => (
+        <FormControl variant="outlined">
+          <InputLabel error={!!error} htmlFor="password">
+            Password
+          </InputLabel>
+          <OutlinedInput
+            id="password"
+            type={values.showPassword ? 'text' : 'password'}
+            value={values.password}
+            onChange={(e) => {
+              handleChange('password')(e);
+              onChange(e);
+            }}
+            error={!!error}
+            onBlur={onBlur}
+            ref={ref}
+            name={name}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Password"
+          />
+          <FormHelperText error={!!error}>
+            {error ? error.message : ' '}
+          </FormHelperText>
+        </FormControl>
+      )}
+    />
+  );
+}
+
+export default PasswordInput;
