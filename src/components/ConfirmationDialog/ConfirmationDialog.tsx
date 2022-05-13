e9420a4101e04ec0561a42190a2949eadc51fd44
wunlei <confusioncosmos@gmail.com>
2022-05-13 04:45:07 +0300

    feat: add confirmation and modal dialogs

---
+++ src/components/ConfirmationDialog/ConfirmationDialog.tsx
@@ -0,0 +1,48 @@
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+} from '@mui/material';
+import { ConfirmationDialogProps } from './ConfirmationDialog.types';
+
+function ConfirmationDialog(props: ConfirmationDialogProps) {
+  const { open, dialogText, onConfirm, onReject } = props;
+
+  return (
+    <Dialog
+      open={open}
+      PaperProps={{
+        sx: {
+          boxShadow: 0,
+          padding: 2,
+          borderRadius: '1rem',
+        },
+      }}
+      aria-labelledby="confirmation-dialog-title"
+      aria-describedby="confirmation-dialog-description"
+    >
+      <DialogTitle
+        id="confirmation-dialog-title"
+        fontWeight="bold"
+        variant="h4"
+        textAlign="center"
+      >
+        Confirm Action
+      </DialogTitle>
+      <DialogContent id="confirmation-dialog-description">
+        {dialogText}
+      </DialogContent>
+      <DialogActions sx={{ justifyContent: 'space-between' }}>
+        <Button onClick={onConfirm} color="success" variant="outlined">
+          Confirm
+        </Button>
+        <Button onClick={onReject} color="error" variant="contained">
+          Cancel
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
+export default ConfirmationDialog;
