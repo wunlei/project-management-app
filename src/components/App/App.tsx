56b9f94641f6ef36a1f7100e853fd8abcad52038
wunlei <confusioncosmos@gmail.com>
2022-05-14 20:31:53 +0300

    feat: update app layout

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Footer from 'components/Footer/Footer';
+import Header from 'components/Header/Header';
+import Stack from '@mui/material/Stack';
+import { team } from 'constants/appConstants';
 
 function App() {
   return (
     <div className="App">
-      <Outlet />
+      <Stack height="100vh" justifyContent="space-between">
+        <Header />
+        <Outlet />
+        <Footer data={team} />
+      </Stack>
     </div>
   );
 }
--- src/constants/appConstants.ts
+++ src/constants/appConstants.ts
@@ -1,1 +1,16 @@
+export const team = [
+  {
+    url: 'https://github.com/nosbog',
+    name: 'nosbog',
+  },
+  {
+    url: 'https://github.com/ninox14',
+    name: 'ninox14',
+  },
+  {
+    url: 'https://github.com/wunlei',
+    name: 'wunlei',
+  },
+];
+
 export const DEFAULT_LANG = 'EN';
