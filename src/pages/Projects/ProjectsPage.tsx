2e12690b63c464d99abda4e9389a10a94e9f28ff
wunlei <confusioncosmos@gmail.com>
2022-05-14 20:29:52 +0300

    feat: update ProjectsPage localization

--- src/pages/Projects/ProjectsPage.tsx
+++ src/pages/Projects/ProjectsPage.tsx
@@ -1,34 +1,34 @@
 import { Box, Container, Stack, Typography } from '@mui/material';
-
+import { useTranslation } from 'react-i18next';
 import ProjectCard from 'components/ProjectCard/ProjectCard';
 import SearchBar from 'components/SearchBar/SearchBar';
 
 function ProjectsPage() {
+  const { t } = useTranslation();
+
   return (
-    <Stack minHeight="100vh">
-      <Container component="main" sx={{ flexGrow: 1 }}>
-        <Stack spacing={3}>
-          <Typography variant="h3" fontWeight="bold">
-            Projects
-          </Typography>
-          <SearchBar />
-          <Box
-            sx={{
-              display: 'flex',
-              flexWrap: 'wrap',
-              columnGap: '1rem',
-              rowGap: '1rem',
-            }}
-          >
-            <ProjectCard
-              title={'My Project #1'}
-              description={'Project description'}
-              boardId={'1'}
-            ></ProjectCard>
-          </Box>
-        </Stack>
-      </Container>
-    </Stack>
+    <Container component="main" sx={{ flexGrow: 1 }}>
+      <Stack spacing={3}>
+        <Typography variant="h3" fontWeight="bold">
+          {t('Projects')}
+        </Typography>
+        <SearchBar />
+        <Box
+          sx={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            columnGap: '1rem',
+            rowGap: '1rem',
+          }}
+        >
+          <ProjectCard
+            title={'My Project #1'}
+            description={'Project description'}
+            boardId={'1'}
+          ></ProjectCard>
+        </Box>
+      </Stack>
+    </Container>
   );
 }
 
