07c500f9500b99ff92bb1ca1410f09b65df93c40
wunlei <confusioncosmos@gmail.com>
2022-05-16 02:43:00 +0300

    feat: add layout to AuthPage

--- src/pages/Auth/AuthPage.tsx
+++ src/pages/Auth/AuthPage.tsx
@@ -1,11 +1,53 @@
 import { Outlet } from 'react-router-dom';
+import { Stack, Link, Typography } from '@mui/material';
+import bg from 'assets/img/towfiqu-barbhuiya-jOeh3Lv88xA-unsplash.jpg';
 
 function AuthPage() {
   return (
-    <div>
-      Auth Page
-      <Outlet />
-    </div>
+    <Stack
+      height="100vh"
+      sx={{
+        position: 'relative',
+        backgroundImage: `url(${bg})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center center',
+        backgroundRepeat: 'no-repeat',
+      }}
+    >
+      <Stack
+        component="main"
+        bgcolor="white"
+        flex={1}
+        alignItems="center"
+        justifyContent="center"
+        width="100%"
+        sx={{
+          zIndex: 1,
+          width: {
+            md: '700px',
+          },
+        }}
+      >
+        <Outlet />
+      </Stack>
+      <Typography
+        variant="body2"
+        sx={{
+          position: 'absolute',
+          right: '0.5rem',
+          bottom: '0.5rem',
+        }}
+      >
+        Photo by{' '}
+        <Link
+          href="https://unsplash.com/@towfiqu999999"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Towfiqu barbhuiya
+        </Link>
+      </Typography>
+    </Stack>
   );
 }
 
