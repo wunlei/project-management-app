27a6226d638b44a45595d3aff0b7a9b10b940dce
NiNoX14 <hardstuckd5lul@gmail.com>
2022-05-19 00:04:14 +0300

    feat: redirect from AuthPages to main route if user is logged in

--- src/pages/Auth/AuthPage.tsx
+++ src/pages/Auth/AuthPage.tsx
@@ -1,6 +1,18 @@
-import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
+import { useAppSelector } from 'redux/hooks';
 
 function AuthPage() {
+  const isLoggedIn = useAppSelector((state) => state.global.userId);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      navigate('/projects');
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div>
       Auth Page
