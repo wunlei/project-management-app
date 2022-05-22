726c16eef624dde5695b7ec0e966169b9e69b19d
Bogdan <noskovbogdan@gmail.com>
2022-05-22 21:49:38 +0300

    feat: BoardColumn uses dnd lib

--- src/components/BoardColumn/BoardColumn.types.ts
+++ src/components/BoardColumn/BoardColumn.types.ts
@@ -1,4 +1,13 @@
+import {
+  DraggableProvidedDraggableProps,
+  DraggableProvidedDragHandleProps,
+} from 'react-beautiful-dnd';
+
 export interface BoardColumnProps {
   children?: React.ReactNode;
   title: string;
+  innerRef: () => void;
+  draggableProps: DraggableProvidedDraggableProps;
+  dragHandleProps: DraggableProvidedDragHandleProps | undefined;
+  id: string;
 }
