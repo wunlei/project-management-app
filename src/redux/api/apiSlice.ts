35e9dea85b1f74e9a6ff20e9d3ef58e82d2c8a3a
Bogdan <noskovbogdan@gmail.com>
2022-05-25 23:38:16 +0300

    feat: handle unauthorized user (expired token)

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -1,23 +1,46 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import {
+  BaseQueryFn,
+  FetchArgs,
+  FetchBaseQueryError,
+} from '@reduxjs/toolkit/query';
 import getToken from 'utils/api/getToken';
+import { setUserId } from '../global/globalSlice';
 
 export const baseUrl = 'http://localhost:4000';
 
+const baseQuery = fetchBaseQuery({
+  baseUrl,
+  prepareHeaders: (headers, { endpoint }) => {
+    const endpointsWithoutToken = ['signIn', 'signUp'];
+
+    if (endpointsWithoutToken.includes(endpoint)) {
+      return headers;
+    }
+
+    headers.set('authorization', `Bearer ${getToken()}`);
+    return headers;
+  },
+});
+
+const baseQueryWithErrorHandlers: BaseQueryFn<
+  string | FetchArgs,
+  unknown,
+  FetchBaseQueryError
+> = async (args, api, extraOptions) => {
+  const result = await baseQuery(args, api, extraOptions);
+
+  if (result.error && result.error.status === 401) {
+    api.dispatch(setUserId(null));
+    localStorage.removeItem('token');
+  }
+
+  return result;
+};
+
 export const apiSlice = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({
-    baseUrl,
-    prepareHeaders: (headers, { endpoint }) => {
-      const endpointsWithoutToken = ['signIn', 'signUp'];
-
-      if (endpointsWithoutToken.includes(endpoint)) {
-        return headers;
-      }
-
-      headers.set('authorization', `Bearer ${getToken()}`);
-      return headers;
-    },
-  }),
+  baseQuery: baseQueryWithErrorHandlers,
   tagTypes: [
     'USER',
     'USERS',
