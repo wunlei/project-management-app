5151f6939f621292275cdd9870bc59bc184b1e5e
NiNoX14 <hardstuckd5lul@gmail.com>
2022-05-25 20:04:24 +0300

    feat: alerts on aerror and success and form reset on close and success

--- src/components/CreateTaskForm/CreateTaskForm.tsx
+++ src/components/CreateTaskForm/CreateTaskForm.tsx
@@ -1,10 +1,14 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useGetAllUsersQuery } from 'redux/api/endpoints/users';
 import { useCreateTaskMutation } from 'redux/api/endpoints/tasks';
 
 import { Controller, SubmitHandler, useForm } from 'react-hook-form';
-import { CreateTaskFormValues } from './CreateTaskForm.types';
+import {
+  CreateTaskFormValues,
+  Props,
+  AlertState,
+} from './CreateTaskForm.types';
 
 import { styled } from '@mui/material/styles';
 import grey from '@mui/material/colors/grey';
@@ -16,6 +20,8 @@ import {
   Typography,
   Autocomplete,
   SvgIcon,
+  Snackbar,
+  Alert,
 } from '@mui/material';
 import Modal from 'components/Modal/Modal';
 
@@ -26,13 +32,6 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import { schema } from './CreateTaskForm.validation';
 
 import { useUploadFileMutation } from 'redux/api/endpoints/file';
-
-interface Props {
-  handleClose: () => void;
-  open: boolean;
-  boardId: string;
-  columnId: string;
-}
 
 const Input = styled('input')({
   display: 'none',
@@ -45,6 +44,8 @@ function CreateTaskFormModal(props: Props) {
   const { data: users } = useGetAllUsersQuery();
   const [createTask, createTaskResult] = useCreateTaskMutation();
   const [uploadFile, uploadFileResult] = useUploadFileMutation();
+
+  const [alertState, setAlertState] = useState<AlertState | null>(null);
 
   const {
     control,
@@ -68,6 +69,7 @@ function CreateTaskFormModal(props: Props) {
     description,
     ...restData
   }) => {
+    // console.log({ member, title, description, ...restData });
     createTask({
       boardId,
       columnId,
@@ -79,6 +81,22 @@ function CreateTaskFormModal(props: Props) {
     });
   };
 
+  const handleCloseAndResetForm = () => {
+    handleClose();
+    reset();
+    createTaskResult.reset();
+    uploadFileResult.reset();
+  };
+
+  const handleAlertClose = () => {
+    setAlertState(null);
+  };
+
+  const onSuccess = () => {
+    setAlertState({ message: 'Successfuly created task!', color: 'success' });
+    handleCloseAndResetForm();
+  };
+
   useEffect(() => {
     const isSuccessfullyCreatedTask =
       createTaskResult.isSuccess && createTaskResult.data;
@@ -87,160 +105,196 @@ function CreateTaskFormModal(props: Props) {
 
     if (shouldUploadFile) {
       uploadFile({ taskId: createTaskResult.data.id, file: fileInputValue[0] });
+    } else {
+      onSuccess();
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [createTaskResult.isSuccess]);
 
+  useEffect(() => {
+    if (uploadFileResult.isSuccess) {
+      onSuccess();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [uploadFileResult.isSuccess]);
+
+  useEffect(() => {
+    if (uploadFileResult.isError || createTaskResult.isError) {
+      setAlertState({ message: 'Something went wrong!', color: 'error' });
+    }
+  }, [uploadFileResult.isError, createTaskResult.isError]);
+
   return (
-    <Modal
-      onClose={() => {
-        handleClose();
-        reset();
-        createTaskResult.reset();
-        uploadFileResult.reset();
-      }}
-      open={open}
-      dialogTitle={t('Create new task')}
-      confirmBtnText={t('Create')}
-      onConfirm={handleSubmit(onSubmit)}
-    >
-      <Stack
-        component="form"
-        onSubmit={handleSubmit(onSubmit)}
-        sx={{ rowGap: 3 }}
+    <>
+      <Snackbar
+        open={!!alertState}
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+        autoHideDuration={5000}
+        onClose={handleAlertClose}
       >
-        <Controller
-          control={control}
-          name="title"
-          defaultValue=""
-          render={({ field: { ref, ...restField }, fieldState: { error } }) => (
-            <TextField
-              inputRef={ref}
-              {...restField}
-              label={t('Task name')}
-              required
-              error={!!error?.message}
-              helperText={
-                error?.message
-                  ? t(error.message, { ns: 'validation' })
-                  : t('Example: Buy milk')
-              }
-            />
-          )}
-        />
-        <Controller
-          control={control}
-          name="description"
-          defaultValue=""
-          render={({ field: { ref, ...restField }, fieldState: { error } }) => (
-            <TextField
-              multiline
-              minRows={3}
-              maxRows={5}
-              sx={{
-                '.MuiInputBase-inputMultiline': {
-                  scrollbarColor: `${grey[400]} ${grey[200]}`,
-                  scrollbarWidth: 'thin',
-                  '&::-webkit-scrollbar': {
-                    width: '10px',
-                    left: '-10px',
-                  },
-                  '&::-webkit-scrollbar-track': {
-                    backgroundColor: grey[200],
-                  },
-                  '&::-webkit-scrollbar-thumb': {
-                    backgroundColor: grey[400],
-                  },
-                },
-              }}
-              inputRef={ref}
-              {...restField}
-              error={!!error?.message}
-              helperText={
-                error?.message
-                  ? t(error.message, { ns: 'validation' })
-                  : t('Example: Dont forget a bag')
-              }
-              label={t('Task description')}
-            />
-          )}
-        />
-        <label htmlFor="contained-button-file">
-          <Input
-            accept=".png, .jpg, .jpeg"
-            id="contained-button-file"
-            multiple={false}
-            type="file"
-            {...register('picture')}
+        {alertState ? (
+          <Alert
+            onClose={handleAlertClose}
+            severity={alertState.color}
+            sx={{ width: '100%' }}
+          >
+            {t(alertState.message)}
+          </Alert>
+        ) : undefined}
+      </Snackbar>
+      <Modal
+        onClose={handleCloseAndResetForm}
+        open={open}
+        dialogTitle={t('Create new task')}
+        confirmBtnText={t('Create')}
+        onConfirm={handleSubmit(onSubmit)}
+      >
+        <Stack
+          component="form"
+          onSubmit={handleSubmit(onSubmit)}
+          sx={{ rowGap: 3 }}
+        >
+          <Controller
+            control={control}
+            name="title"
+            defaultValue=""
+            render={({
+              field: { ref, ...restField },
+              fieldState: { error },
+            }) => (
+              <TextField
+                inputRef={ref}
+                {...restField}
+                label={t('Task name')}
+                required
+                error={!!error?.message}
+                helperText={
+                  error?.message
+                    ? t(error.message, { ns: 'validation' })
+                    : t('Example: Buy milk')
+                }
+              />
+            )}
           />
-          <Button
-            variant="contained"
-            component="span"
-            color={
-              isCorrectFileSelected
-                ? 'success'
-                : errors.picture
-                ? 'error'
-                : 'primary'
-            }
-            startIcon={
-              isCorrectFileSelected ? (
-                <SvgIcon>
-                  <CheckIcon />
-                </SvgIcon>
-              ) : (
-                <FileIcon />
-              )
-            }
-          >
-            {t('Upload image')}
-          </Button>
-          <Typography
-            color={errors.picture ? 'error' : 'primary'}
-            sx={{
-              opacity: errors.picture ? 1 : 0.7,
-            }}
-          >
-            {t('Max file size is 1mb')}
-          </Typography>
-        </label>
-        <Controller
-          control={control}
-          name="member"
-          defaultValue={{ id: '', name: '', login: '' }}
-          render={({
-            field: { onChange, value, name },
-            fieldState: { error },
-          }) => (
-            <Autocomplete
-              fullWidth
-              id="create-task-combo-box"
-              onChange={(event, item) => {
-                onChange(item);
-              }}
-              value={value ?? null}
-              isOptionEqualToValue={(option, value) =>
-                option.login === value.login || value.login === ''
-              }
-              options={users ? [...users] : []}
-              getOptionLabel={(user) => user.login}
-              renderInput={(params) => (
-                <TextField
-                  {...params}
-                  name={name}
-                  error={!!error?.message}
-                  required
-                  helperText={
-                    error?.message ? t(error.message, { ns: 'validation' }) : ''
-                  }
-                  label={t('Select member')}
-                />
-              )}
+          <Controller
+            control={control}
+            name="description"
+            defaultValue=""
+            render={({
+              field: { ref, ...restField },
+              fieldState: { error },
+            }) => (
+              <TextField
+                multiline
+                minRows={3}
+                maxRows={5}
+                sx={{
+                  '.MuiInputBase-inputMultiline': {
+                    scrollbarColor: `${grey[400]} ${grey[200]}`,
+                    scrollbarWidth: 'thin',
+                    '&::-webkit-scrollbar': {
+                      width: '10px',
+                      left: '-10px',
+                    },
+                    '&::-webkit-scrollbar-track': {
+                      backgroundColor: grey[200],
+                    },
+                    '&::-webkit-scrollbar-thumb': {
+                      backgroundColor: grey[400],
+                    },
+                  },
+                }}
+                inputRef={ref}
+                {...restField}
+                error={!!error?.message}
+                helperText={
+                  error?.message
+                    ? t(error.message, { ns: 'validation' })
+                    : t('Example: Dont forget a bag')
+                }
+                label={t('Task description')}
+              />
+            )}
+          />
+          <label htmlFor="contained-button-file">
+            <Input
+              accept=".png, .jpg, .jpeg"
+              id="contained-button-file"
+              multiple={false}
+              type="file"
+              {...register('picture')}
             />
-          )}
-        />
-      </Stack>
-    </Modal>
+            <Button
+              variant="contained"
+              component="span"
+              color={
+                isCorrectFileSelected
+                  ? 'success'
+                  : errors.picture
+                  ? 'error'
+                  : 'primary'
+              }
+              startIcon={
+                isCorrectFileSelected ? (
+                  <SvgIcon>
+                    <CheckIcon />
+                  </SvgIcon>
+                ) : (
+                  <FileIcon />
+                )
+              }
+            >
+              {t('Upload image')}
+            </Button>
+            <Typography
+              color={errors.picture ? 'error' : 'primary'}
+              sx={{
+                opacity: errors.picture ? 1 : 0.7,
+              }}
+            >
+              {t('Max file size is 1mb')}
+            </Typography>
+          </label>
+          <Controller
+            control={control}
+            name="member"
+            defaultValue={{ id: '', name: '', login: '' }}
+            render={({
+              field: { onChange, value, name },
+              fieldState: { error },
+            }) => (
+              <Autocomplete
+                fullWidth
+                id="create-task-combo-box"
+                onChange={(event, item) => {
+                  onChange(item);
+                }}
+                value={value ?? null}
+                isOptionEqualToValue={(option, value) =>
+                  option.login === value.login || value.login === ''
+                }
+                options={users ? [...users] : []}
+                getOptionLabel={(user) => user.login}
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    name={name}
+                    error={!!error?.message}
+                    required
+                    helperText={
+                      error?.message
+                        ? t(error.message, { ns: 'validation' })
+                        : ''
+                    }
+                    label={t('Select member')}
+                  />
+                )}
+              />
+            )}
+          />
+        </Stack>
+      </Modal>
+    </>
   );
 }
 
--- src/components/CreateTaskForm/CreateTaskForm.types.ts
+++ src/components/CreateTaskForm/CreateTaskForm.types.ts
@@ -1,5 +1,17 @@
 import { UserFromServer } from 'redux/api/apiTypes';
 
+export interface Props {
+  handleClose: () => void;
+  open: boolean;
+  boardId: string;
+  columnId: string;
+}
+
+export interface AlertState {
+  message: string;
+  color: 'success' | 'error';
+}
+
 export interface CreateTaskFormValues {
   title: string;
   description: string;
