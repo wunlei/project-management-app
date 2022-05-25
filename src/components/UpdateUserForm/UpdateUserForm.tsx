7a38652487f42968e7c30b35822386f115c47ddc
wunlei <confusioncosmos@gmail.com>
2022-05-26 02:36:40 +0300

    feat: update app alerts

--- src/components/UpdateUserForm/UpdateUserForm.tsx
+++ src/components/UpdateUserForm/UpdateUserForm.tsx
@@ -1,5 +1,5 @@
-import { useState, useEffect } from 'react';
-import { useAppSelector } from 'redux/hooks';
+import { useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from 'redux/hooks';
 import {
   useGetUserQuery,
   useUpdateUserMutation,
@@ -9,26 +9,24 @@ import { useForm, Controller, SubmitHandler } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { userValidationSchema } from 'constants/validation';
 
-import { EditProfile, AlertState } from './UpdateUserForm.types';
+import { EditProfile } from './UpdateUserForm.types';
 
-import {
-  Alert,
-  Button,
-  Skeleton,
-  Snackbar,
-  Stack,
-  TextField,
-} from '@mui/material';
+import { Button, Skeleton, Stack, TextField } from '@mui/material';
 
 import PasswordInput from 'components/PasswordInput/PasswordInput';
+import { setAlertState } from 'redux/global/globalSlice';
 
 function UpdateUserForm() {
   const { t } = useTranslation();
+  const dispatch = useAppDispatch();
+
   const userId = useAppSelector((state) => state.global.userId);
 
-  const [alertState, setAlertState] = useState<AlertState | null>(null);
-
-  const { data: user, isLoading } = useGetUserQuery({
+  const {
+    data: user,
+    isLoading,
+    isError,
+  } = useGetUserQuery({
     userId: userId ?? undefined,
   });
   const [updateUser, updateUserResult] = useUpdateUserMutation();
@@ -46,32 +44,36 @@ function UpdateUserForm() {
     updateUser({ userId: userId ?? undefined, body: data });
   };
 
-  const handleAlertClose = (
-    event?: React.SyntheticEvent | Event,
-    reason?: string
-  ) => {
-    if (reason === 'clickaway') {
-      return;
-    }
-
-    setAlertState(null);
-  };
-
   useEffect(() => {
     if (updateUserResult.isSuccess) {
-      setAlertState({
-        color: 'success',
-        message: t('Successfully updated profile!'),
-      });
+      dispatch(
+        setAlertState({
+          alertMessage: 'Successfully updated profile!',
+          alertType: 'success',
+        })
+      );
     }
     if (updateUserResult.isError) {
-      setAlertState({
-        color: 'error',
-        message: t('Something went wrong while updating profile!'),
-      });
+      dispatch(
+        setAlertState({
+          alertMessage: 'Something went wrong while updating profile!',
+          alertType: 'error',
+        })
+      );
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [updateUserResult]);
+  useEffect(() => {
+    if (isError) {
+      dispatch(
+        setAlertState({
+          alertMessage: 'Something went wrong while updating profile!',
+          alertType: 'error',
+        })
+      );
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isError]);
 
   return (
     <>
@@ -157,22 +159,6 @@ function UpdateUserForm() {
           {t('Save')}
         </Button>
       </Stack>
-      <Snackbar
-        open={!!alertState}
-        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
-        autoHideDuration={5000}
-        onClose={handleAlertClose}
-      >
-        {alertState ? (
-          <Alert
-            onClose={handleAlertClose}
-            severity={alertState.color}
-            sx={{ width: '100%' }}
-          >
-            {alertState.message}
-          </Alert>
-        ) : undefined}
-      </Snackbar>
     </>
   );
 }
